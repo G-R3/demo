d7a8c62eba548f59b75dbeef888f31fd42f651fe
Gerardo <gr261212@gmail.com>
2022-02-14 18:50:45 -0500

    update drowndown menu

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 
 export default function Dropdown() {
     const [isOpen, setIsOpen] = useState(false);
+    const [val, setVal] = useState("");
+    const dropdownValues = [
+        {
+            title: "Dijkstra",
+        },
+        {
+            title: "A* Pathfinding",
+        },
+    ];
+
     return (
         <div className="nav-dropdown">
             <button
                 className="btn nav-dropdown-toggle"
                 onClick={() => setIsOpen(!isOpen)}
             >
-                Choose Alogirthm
+                {val ? val : "Choose Algorithm"}
             </button>
 
             {isOpen && (
                 <ul className="dropdown-options">
-                    <li>Algorithm 1</li>
-                    <li>Algorithm 2</li>
-                    <li>Algorithm 3</li>
+                    {dropdownValues.map((val, i) => (
+                        <li key={i} onClick={() => setVal(val.title)}>
+                            {val.title}
+                        </li>
+                    ))}
                 </ul>
             )}
         </div>
