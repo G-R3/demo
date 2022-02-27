297630bb593e5a5f7621924494ab159b4600b486
Gerardo <gr261212@gmail.com>
2022-02-27 18:46:25 -0500

    replaced file extension

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <p className="nav-title">Visualizer</p>
+            <button className="btn nav-button">Visualize</button>
+        </nav>
+    );
+}
